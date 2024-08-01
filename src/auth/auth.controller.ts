3f1ed4773bc92a47a2201bad9e71fe0e69d2ea89
BASIL AST <basilast@BASILs-MacBook-Pro.local>
2024-08-01 13:00:25 +0100

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post } from "@nestjs/common"
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    // authService: AuthService;
+
+    constructor(private authService: AuthService) { }
+    //this.authService = authService
+
+    @Post('signup')
+    signup() {
+        return this.authService.signup()
+    }
+
+    @Post('signin')
+    login() {
+        return this.authService.signin()
+    }
+}
