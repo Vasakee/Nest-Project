4fd0c1fa40bb8376ee6ba4746f335b91d09573ab
Basil Dayigil <97135216+Vasakee@users.noreply.github.com>
2024-08-03 04:48:16 +0000

    newest commit

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,15 @@
+import { Delete, Get, Injectable, Patch, Post } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+
+@Injectable()
+export class BookmarkService {
+    getBookmarks(userId: number) {}
+
+    createBookmark(userId: number, dto: CreateBookmarkDto){}
+    
+    getBookmarkById(userId:number, bookmarkId: number) {}
+
+    editBookmarkById(userId:number, bookmarkId:number, dto: EditBookmarkDto) {}
+
+    deleteBookmarkById(userId: number, bookmarkId:number) {}
+}
--- src/user/dto/edit-user.dto.ts
+++ src/user/dto/edit-user.dto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsOptional, IsString } from "class-validator"
 
-class EditUserDto {
-    email: string
+export class EditUserDto {
+    @IsEmail()
+    @IsOptional()
+    email?: string
+
+    @IsString()
+    @IsOptional()
     firstName?: string
-    las
+
+    @IsString()
+    @IsOptional()
+    lastName?: string 
 }
